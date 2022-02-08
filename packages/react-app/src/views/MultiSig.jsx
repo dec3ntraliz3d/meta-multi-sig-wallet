9d99ea4cc36448a2b37c8eb529f065c80ddcdde0
dec3ntraliz3d <sa6210441@pm.me>
2022-02-08 12:24:00 -0500

    Cleaned up commented code .

--- packages/react-app/src/views/MultiSig.jsx
+++ packages/react-app/src/views/MultiSig.jsx
@@ -5,12 +5,6 @@ import { Balance, Address, TransactionListItem } from "../components";
 import QR from "qrcode.react";
 import { List } from "antd/lib/form/Form";
 
-/**
- * web3 props can be passed from '../App.jsx' into your local view component for use
- * @param {*} yourLocalBalance balance on current network
- * @param {*} readContracts contracts from current chain already pre-loaded using ethers contract module. More here https://docs.ethers.io/v5/api/contract/contract/
- * @returns react component
- **/
 function MultiSig({
   contractAddress,
   localProvider,
@@ -21,9 +15,6 @@ function MultiSig({
   contractName,
   readContracts
 }) {
-  // you can also use hooks locally in your component of choice
-  // in this case, let's keep track of 'purpose' variable from our contract
-
 
   return (
     <div style={{ padding: 32, maxWidth: 750, margin: "auto" }}>
@@ -67,26 +58,7 @@ function MultiSig({
           );
         }}
       >
-
       </List>
-
-
-
-
-      {/* <List
-        bordered
-        dataSource={executeTransactionEvents}
-        renderItem={item => {
-
-          return (
-            <>
-              <TransactionListItem item={item} mainnetProvider={mainnetProvider} blockExplorer={blockExplorer} price={price} readContracts={readContracts} contractName={contractName} />
-            </>
-          );
-        }}
-      /> */}
-
-
     </div>
   );
 }
